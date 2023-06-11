6e8c2754048e8d2ebbeb1a312ef32c1d0a5ca4b2
Nibaldo Quezada <nibaldoquezada@hotmail.com>
2023-06-10 21:00:14 -0400

    Subiendo index y contacto

---
+++ assets/js/index.js
@@ -0,0 +1,33 @@
+$(document).ready( function () {
+    $('#myTable').DataTable();
+} );
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("contacto").addEventListener('submit', validarFormulario); 
+    });
+    
+    function validarFormulario(evento) {
+    
+        evento.preventDefault();
+        let name = document.getElementById('nombre').value;
+        if(name.length == 0) {
+        alert('Escriba su nombre');
+        return;
+        }
+        let lastname = document.getElementById('apellido').value;
+        if(lastname.length == 0) {
+        alert('Escriba su apellido');
+        return;
+        }
+        let email = document.getElementById('correo').value;
+        if(email.length == 0) {
+            alert('Escriba su  email');
+            return;
+        }
+        let message = document.getElementById('mensaje').value;
+        if (message.length == 0 ) {
+        alert('Escriba su mensaje');
+        return;
+        }
+        this.submit();
+    }
